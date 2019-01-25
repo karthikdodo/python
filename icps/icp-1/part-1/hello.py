4dec8da4604986551212a35ffcb096413cc0cfe9
karthik gundlapalli <gundlapalli.karthik@gmail.com>
2019-01-25 17:50:35 -0600

    icp-1

---
+++ icps/icp-1/part-1/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print ("Hello, Python!")
+
+s = input("Input a string")
+d=l=0
+for c in s:
+    if c.isdigit():
+        d=d+1
+    elif c.isalpha():
+        l=l+1
+    else:
+        pass
+print("Letters", l)
+print("Digits", d)
